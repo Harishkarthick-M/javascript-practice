dc31ff77e7ac0cf0223687614690706f772c8ffc
Harish-karthick <harish.maheswaran@fssa.freshworks.com>
2024-09-09 16:26:47 +0530

    tables commit

---
+++ tables.js
@@ -0,0 +1,21 @@
+//tables
+
+let i = 1; 
+let number = 4; 
+while (i <= 12) { 
+    let num = number * i;
+        console.log(`${number} * ${i} = ${num}`);
+    i++; 
+}
+
+//odd or even only need to print
+// chnage I to i
+let I = 1; 
+let numb = 4; 
+while (i <= 12) { 
+    let num = number * I;
+    if (I % 2 === 1) {  
+        console.log(`${number} * ${I} = ${num}`);
+    }
+    i++; 
+}
